b014ceff1df953e4a04c46eac9aa6e6a322db15e
hsjhita1 <hjhita1997@gmail.com>
2020-06-02 13:22:40 +0000

    Created inital testing for TDD approach

---
+++ Service3/tests/test_difficulty.py
@@ -0,0 +1,15 @@
+import pytest, unittest
+from application import app, routes
+from flask import url_for
+from flask_testing import TestCase
+from os import getenv
+
+class TestBase(TestCase):
+    def create_app(self):
+        config_name = 'test'
+        return app
+
+class TestApp(TestBase):
+    def test_home_page(self):
+        response = self.client.get(url_for('service3'))
+        self.assertEqual(response.status_code, 200)
