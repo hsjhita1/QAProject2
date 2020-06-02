33d02156ca769e296a8e8455777eb610d96d8a02
hsjhita1 <hjhita1997@gmail.com>
2020-06-02 15:31:47 +0000

    Added database for storing results

--- Service1/application/__init__.py
+++ Service1/application/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('FLASK_DB_URI')
+
+db = SQLAlchemy(app)
 
 from application import routes
