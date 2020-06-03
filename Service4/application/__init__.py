cb06e7787fa9eec843cbe3cbe994d99e46a963af
hsjhita1 <hjhita1997@gmail.com>
2020-06-03 16:15:24 +0000

    Changed DB reference to remove confusion with other projects

--- Service4/application/__init__.py
+++ Service4/application/__init__.py
@@ -3,7 +3,7 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('FLASK_DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('PROJECT2_DB')
 
 db = SQLAlchemy(app)
 
