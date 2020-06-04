0b23a0e53e53a85e59efb1ce5d8d4ab53235d141
hsjhita1 <hjhita1997@gmail.com>
2020-06-04 18:59:14 +0100

    More testing

--- Service4/tests/test_service4.py
+++ Service4/tests/test_service4.py
@@ -1,5 +1,6 @@
 import pytest, unittest
-from application import app, routes
+from application import app, db, routes
+from application.models import results
 from flask import url_for
 from flask_testing import TestCase
 from os import getenv
@@ -7,7 +8,19 @@ from os import getenv
 class TestBase(TestCase):
     def create_app(self):
         config_name = 'test'
+        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TEST_DB_URI'),
+            DEBUG=True
+        )
         return app
+
+    def setUp(self):
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 class TestApp(TestBase):
     def test_home_page(self):
